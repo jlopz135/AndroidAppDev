7e56170807b5d8e09a2d28ca73322067d63d881c
Juan Lopez <juanclopez135@gmail.com>
2023-01-31 16:54:55 -0500

    Adding TapCounter Files

---
+++ TapCounter/app/src/main/java/com/example/tapcounter/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.tapcounter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.buttonClicker)
+
+        button.setOnClickListener {
+            Toast.makeText(it.context, "Clicked Button!", Toast.LENGTH_SHORT).show()
+        }
+
+        val textView = findViewById<TextView>(R.id.textView3)
+        var counter = 0
+
+        val upgradeButton = findViewById<Button>(R.id.upgradeBtn)
+
+        button.setOnClickListener {
+            counter++
+            textView.text = counter.toString()
+
+            /*-------- Stretch Feature ---------*/
+            if (counter >= 100) {
+
+                upgradeButton.visibility = View.VISIBLE
+                upgradeButton.setOnClickListener {
+                    button.text = "Add 2"
+
+                    button.setOnClickListener {
+                        counter += 2
+                        textView.text = counter.toString()
+                    }
+                    upgradeButton.visibility = View.INVISIBLE
+                }
+            }
+        }
+    }
+
+}
